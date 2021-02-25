ebce77a1314bcaccd842b8f65086292d4c18ea14
sumit619sharma <imsumitsharma619@gmail.com>
2021-02-25 03:37:49 -0800

    social-media app

---
+++ app/src/main/java/com/sumit/socialapp/CreatePost.kt
@@ -0,0 +1,26 @@
+package com.sumit.socialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sumit.socialapp.daos.PostDao
+import kotlinx.android.synthetic.main.activity_create_post.*
+
+class CreatePost : AppCompatActivity() {
+    private lateinit var postDao: PostDao
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+        postDao=PostDao()
+
+        postBtn.setOnClickListener {
+
+            val input = postInput.text.toString().trim()
+            if(input.isNotEmpty()){
+            postDao.addPost(input)
+                finish()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/sumit/socialapp/models/User.kt
@@ -0,0 +1,5 @@
+package com.sumit.socialapp.models
+
+data class User(val uid: String="",
+                val displayName: String="",
+                val imageUrl: String="")
